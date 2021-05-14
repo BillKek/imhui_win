22123109eca395169ded45bda12e718d78bf84da
BillKek <51540758+BillKek@users.noreply.github.com>
2021-05-14 23:14:47 +0300

    parameter

--- fix_brotli_bug.sh
+++ fix_brotli_bug.sh
@@ -1,5 +1,5 @@
 
-cp "D:\a\_temp\msys\msys64\${{ matrix.msystem_lower }}\libbrotlicommon-static.a" "D:\a\_temp\msys\msys64\${{ matrix.msystem_lower }}\libbrotlicommon.a"
-cp "D:\a\_temp\msys\msys64\${{ matrix.msystem_lower }}\libbrotlidec-static.a" "D:\a\_temp\msys\msys64\${{ matrix.msystem_lower }}\libbrotlidec.a"
-cp "D:\a\_temp\msys\msys64\${{ matrix.msystem_lower }}\libbrotlienc-static.a" "D:\a\_temp\msys\msys64\${{ matrix.msystem_lower }}\libbrotlienc.a"
+cp "D:\a\_temp\msys\msys64\$1\libbrotlicommon-static.a" "D:\a\_temp\msys\msys64\${{ matrix.msystem_lower }}\libbrotlicommon.a"
+cp "D:\a\_temp\msys\msys64\$1\libbrotlidec-static.a" "D:\a\_temp\msys\msys64\${{ matrix.msystem_lower }}\libbrotlidec.a"
+cp "D:\a\_temp\msys\msys64\$1\libbrotlienc-static.a" "D:\a\_temp\msys\msys64\${{ matrix.msystem_lower }}\libbrotlienc.a"
 
