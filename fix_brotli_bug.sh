aa5a36977aa4c608a33a184433d5d3f1fb8daaa9
BillKek <51540758+BillKek@users.noreply.github.com>
2021-05-14 23:31:59 +0300

    i am serious !

--- fix_brotli_bug.sh
+++ fix_brotli_bug.sh
@@ -1,5 +1,5 @@
 
-cp "D:\a\_temp\msys\msys64\\$1\libbrotlicommon-static.a" "D:\a\_temp\msys\msys64\\$1\libbrotlicommon.a"
-cp "D:\a\_temp\msys\msys64\\$1\libbrotlidec-static.a" "D:\a\_temp\msys\msys64\\$1\libbrotlidec.a"
-cp "D:\a\_temp\msys\msys64\\$1\libbrotlienc-static.a" "D:\a\_temp\msys\msys64\\$1\libbrotlienc.a"
+cp "D:\a\_temp\msys\msys64\\$1\lib\libbrotlicommon-static.a" "D:\a\_temp\msys\msys64\\$1\lib\libbrotlicommon.a"
+cp "D:\a\_temp\msys\msys64\\$1\lib\libbrotlidec-static.a" "D:\a\_temp\msys\msys64\\$1\lib\libbrotlidec.a"
+cp "D:\a\_temp\msys\msys64\\$1\lib\libbrotlienc-static.a" "D:\a\_temp\msys\msys64\\$1\lib\libbrotlienc.a"
 
