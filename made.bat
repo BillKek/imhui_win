7da3e43c17cad230e64b052f4bc5b2e16adb8b8e
BillKek <51540758+BillKek@users.noreply.github.com>
2021-05-14 20:47:39 +0300

    path2

--- made.bat
+++ made.bat
@@ -1,1 +1,2 @@
-C:\msys64\msys2_shell.cmd -mingw64 -here -c "mingw32-make.exe -B 2>err.txt"
+SET PATH=C:\msys64;C:\msys64\mingw64\bin;C:\msys64\usr
+msys2_shell.cmd -mingw64 -here -c "mingw32-make.exe -B 2>err.txt >mess.txt"
