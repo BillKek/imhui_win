254246a7148a83cb51d276e56aa3b331029b86b1
BillKek <51540758+BillKek@users.noreply.github.com>
2021-05-14 23:09:05 +0300

    sh ! not cmd !

--- fix_brotli_bug.sh
+++ fix_brotli_bug.sh
@@ -1,5 +1,5 @@
 
-copy /B /Y "D:\a\_temp\msys\msys64\${{ matrix.msystem_lower }}\libbrotlicommon-static.a" "D:\a\_temp\msys\msys64\${{ matrix.msystem_lower }}\libbrotlicommon.a"
-copy /B /Y "D:\a\_temp\msys\msys64\${{ matrix.msystem_lower }}\libbrotlidec-static.a" "D:\a\_temp\msys\msys64\${{ matrix.msystem_lower }}\libbrotlidec.a"
-copy /B /Y "D:\a\_temp\msys\msys64\${{ matrix.msystem_lower }}\libbrotlienc-static.a" "D:\a\_temp\msys\msys64\${{ matrix.msystem_lower }}\libbrotlienc.a"
+cp "D:\a\_temp\msys\msys64\${{ matrix.msystem_lower }}\libbrotlicommon-static.a" "D:\a\_temp\msys\msys64\${{ matrix.msystem_lower }}\libbrotlicommon.a"
+cp "D:\a\_temp\msys\msys64\${{ matrix.msystem_lower }}\libbrotlidec-static.a" "D:\a\_temp\msys\msys64\${{ matrix.msystem_lower }}\libbrotlidec.a"
+cp "D:\a\_temp\msys\msys64\${{ matrix.msystem_lower }}\libbrotlienc-static.a" "D:\a\_temp\msys\msys64\${{ matrix.msystem_lower }}\libbrotlienc.a"
 
